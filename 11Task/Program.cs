40f3d05f07cec38670ebdc060e1dfa6a6100beb0
Dimityr <DimityrDimitrov92@abv.bg>
2021-03-01 16:02:08 +0200

    FirstCommit

--- 11Task/Program.cs
+++ 11Task/Program.cs
@@ -6,23 +6,43 @@ namespace _11Task
     {
         static void Main(string[] args)
         {
-            
+
             Console.Write($"Enter int number : ");
             int num = int.Parse(Console.ReadLine());
-            Console.Write($"Enter 1 to convert in hex or enter 2 to convert in binary :" );
+            Console.Write($"Enter 1 to convert in hex or enter 2 to convert in binary :");
             int choose = int.Parse(Console.ReadLine());
             switch (choose)
             {
                 case 1:
                     string hexString = Convert.ToString(Convert.ToInt32(num), 16);
                     Console.WriteLine(hexString);
-                        break;
+                    break;
                 case 2:
                     string binString = Convert.ToString(Convert.ToInt32(num), 2);
                     Console.WriteLine(binString);
                     break;
- 
+
             }
+            //int s, d;
+
+            //Console.Write("Enter N: ");
+            //string n = Console.ReadLine();
+
+            //do
+            //{
+            //    Console.Write("Enter S (S == 2, 8, 10 or 16): ");
+            //    s = Int32.Parse(Console.ReadLine());
+            //} while (s != 2 && s != 8 && s != 10 && s != 16);
+
+            //do
+            //{
+            //    Console.Write("Enter D (D == 2, 8, 10 or 16): ");
+            //    d = Int32.Parse(Console.ReadLine());
+            //} while (d != 2 && d != 8 && d != 10 && d != 16);
+
+            //n = Convert.ToString(Convert.ToInt32(n, s), d);
+
+            //Console.WriteLine("Result is {0}.", n);
 
 
 
